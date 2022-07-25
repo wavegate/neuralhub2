0c83ab2a0e26104585f0034854325b746ef3efb6
wavegate <cc.frankee@gmail.com>
2022-07-24 21:17:18 -0700

    commit

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
-    body = models.TextField(null=True, blank=True)
+    # body = models.TextField(null=True, blank=True)
+    body = RichTextField(blank=True, null=True)
     date_created = models.DateTimeField(null=True, blank=True)
     date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, blank=True)
