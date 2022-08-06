ebe9f3c2d8ee4085200738eed072ae1545e4a1ca
wavegate <cc.frankee@gmail.com>
2022-08-06 00:43:58 -0700

    commit

--- blog/static/js/prismPrepper.js
+++ blog/static/js/prismPrepper.js
@@ -14,8 +14,8 @@ class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: "",
-      language: "javascript",
+      code: "<p>Hello World!</p>",
+      language: "html",
       result: "",
       preview: "",
       style: "",
@@ -23,6 +23,43 @@ class Form extends React.Component {
       lines: "",
     };
   }
+
+  componentDidMount = async () => {
+    let dataLine = "";
+    if (this.state.lines) {
+      dataLine = ` data-line="${this.state.lines}"`;
+    }
+    let dataFile = "";
+    if (this.state.file) {
+      dataFile = ` data-file="${this.state.file}"`;
+    }
+    let output = escapeHtml(this.state.code);
+    let result = `<pre${dataLine}${dataFile}>
+<code class="language-${this.state.language}">
+${output}
+</code>
+</pre>`;
+    let languageName = "language-" + this.state.language;
+    let preview = (
+      <pre
+        className={languageName}
+        data-file={this.state.file || null}
+        data-line={this.state.lines || null}
+      >
+        <code className={languageName}>{this.state.code}</code>
+      </pre>
+    );
+    if (this.state.style == "inline") {
+      result = `<code class="language-${this.state.language}">
+${output}
+</code>`;
+      preview = <code className={languageName}>{this.state.code}</code>;
+    }
+    await this.setState(
+      { result: result, preview: preview },
+      Prism.highlightAll
+    );
+  };
 
   handleChange = async (event) => {
     const value = event.target.value;
@@ -68,16 +105,17 @@ ${output}
     return (
       <div>
         <form>
-          <div>
-            First, enter the code block or inline snippet you wish to format
-            with Prism.js:
-          </div>
+          <h2>Paste in code to be highlighted</h2>
+          <p>
+            First, enter the code block or inline snippet you wish to highlight.
+            A default example is pre-populated:
+          </p>
           <textarea
             name="code"
             value={this.state.code}
             onChange={this.handleChange}
           />
-          <div>Select the language the code is written in:</div>
+          <p>Select the language the code is written in:</p>
           <select
             value={this.state.language}
             onChange={this.handleChange}
@@ -92,7 +130,7 @@ ${output}
             <option value="cpp">C++</option>
             <option value="java">Java</option>
             <option value="php">PHP</option>
-            <option value="markdown">markdown</option>
+            <option value="markdown">Markdown</option>
             <option value="visual-basic">Visual Basic</option>
             <option value="sql">SQL</option>
             <option value="r">R</option>
@@ -102,7 +140,7 @@ ${output}
             <option value="typescript">TypeScript</option>
             <option value="wolfram">Wolfram language</option>
           </select>
-          <div>Choose if you want the code in block or inline form:</div>
+          <p>Choose if you want the code in block or inline form:</p>
           <select
             value={this.state.style}
             onChange={this.handleChange}
@@ -111,24 +149,25 @@ ${output}
             <option value="block">Block</option>
             <option value="inline">Inline</option>
           </select>
-          <div>
+          <h2>Additional options</h2>
+          <p>
             If the code is associated with a specific file, specify the file
             path here. This creates a small tag in the upper-right corner with
             the file path:
-          </div>
+          </p>
           <input
             type="text"
             name="file"
             value={this.state.file}
             onChange={this.handleChange}
           ></input>
-          <div>
+          <p>
             Enter values here if you want to highlight specific lines. For
             example, if you want to highlight lines 3, 4, 6, 7, 8, then write
             "3-4,6-8". Be careful not to enter any non-numeric characters as
             this currently breaks the page and I haven't gotten around to fixing
             it yet:
-          </div>
+          </p>
           <input
             type="text"
             name="lines"
@@ -136,16 +175,18 @@ ${output}
             onChange={this.handleChange}
           ></input>
         </form>
-        <div>
-          Here is the resulting code you can add to your HTML file. If you're
-          not sure how to use this, please see instructions on how to install
-          Prism.js and the required plugins below.
-        </div>
+        <h2>Code result and preview</h2>
+        <p>
+          Here is the resulting code you can now copy and paste into your HTML
+          file. To be clear, you do need to install Prism.js in order for this
+          code to work. If you're not sure how to do this, please see
+          instructions below on how to do exactly that.
+        </p>
         <textarea value={this.state.result} readOnly></textarea>
-        <div>
+        <p>
           Here's an example of how your code will show up after being processed
           by Prism.js:
-        </div>
+        </p>
         <div>{this.state.preview}</div>
       </div>
     );
